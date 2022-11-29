e8c9c041ef314119db7361fd3923204c8d57d7ae
theISCA <gabriel.escareli@gmail.com>
2022-11-29 12:20:28 -0300

    teste

--- src/test/java/org/eircodeapicached/address/test/AddressURIBuilderTest.java
+++ src/test/java/org/eircodeapicached/address/test/AddressURIBuilderTest.java
@@ -5,9 +5,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 import java.net.URI;
-
 import javax.validation.ConstraintViolationException;
-
 import org.eircodeapicached.address.api.AddressURIBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,81 +18,155 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class AddressURIBuilderTest {
 
-	@Value("${postcoder.address.host}")
-	private String addressLookupHost;
+  @Value("${postcoder.address.host}") private String addressLookupHost;
 
-	@Value("${postcoder.address.path}")
-	private String addressLookupPath;
+  @Value("${postcoder.address.path}") private String addressLookupPath;
 
-	@Autowired
-	private AddressURIBuilder uriBuilder;
+  @Autowired private AddressURIBuilder uriBuilder;
 
-	@Test
-	public void testAddressUrlParameterPassing() throws Exception {
+  @Test
+  public void testAddressUrlParameterPassing() throws Exception {
 
-		URI uri = uriBuilder.generateAddressURIFromUrl(addressLookupHost + addressLookupPath, "apiKey", "ie",
-				"eircodeOrAddressFrag", "format", 1,
-				0, "include", "exclude", "addtags", "identifier", "callback", true);
-		String url = uri.toString();
+    URI uri =
+        uriBuilder.generateAddressURIFromUrl(
+            addressLookupHost + addressLookupPath,
+            "apiKey",
+            "ie",
+            "eircodeOrAddressFrag",
+            "format",
+            1,
+            0,
+            "include",
+            "exclude",
+            "addtags",
+            "identifier",
+            "callback",
+            true);
+    String url = uri.toString();
 
-		assertThat(url, containsString("apiKey"));
-		assertThat(url, containsString("ie"));
-		assertThat(url, containsString("eircodeOrAddressFrag"));
-		assertThat(url, containsString("format=format"));
-		assertThat(url, containsString("lines=1"));
-		assertThat(url, containsString("page=0"));
-		assertThat(url, containsString("include=include"));
-		assertThat(url, containsString("exclude=exclude"));
-		assertThat(url, containsString("addtags=addtags"));
-		assertThat(url, containsString("identifier=identifier"));
-		assertThat(url, containsString("callback=callback"));
-		assertThat(url, containsString("postcodeonly=true"));
-	}
+    assertThat(url, containsString("apiKey"));
+    assertThat(url, containsString("ie"));
+    assertThat(url, containsString("eircodeOrAddressFrag"));
+    assertThat(url, containsString("format=format"));
+    assertThat(url, containsString("lines=1"));
+    assertThat(url, containsString("page=0"));
+    assertThat(url, containsString("include=include"));
+    assertThat(url, containsString("exclude=exclude"));
+    assertThat(url, containsString("addtags=addtags"));
+    assertThat(url, containsString("identifier=identifier"));
+    assertThat(url, containsString("callback=callback"));
+    assertThat(url, containsString("postcodeonly=true"));
+  }
 
-	@Test
-	public void testAddressUrlNoEirCode() throws Exception {
+  @Test
+  public void testAddressUrlNoEirCode() throws Exception {
 
-		URI uri = uriBuilder.generateAddressURIFromUrl(addressLookupHost + addressLookupPath, "apiKey", "ie",
-				null, null, null, null, null, null, null, null, null, null);
-		String url = uri.toString();
+    URI uri =
+        uriBuilder.generateAddressURIFromUrl(
+            addressLookupHost + addressLookupPath,
+            "apiKey",
+            "ie",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    String url = uri.toString();
 
-		assertThat(url, containsString("apiKey"));
-		assertThat(url, containsString("ie"));
-	}
+    assertThat(url, containsString("apiKey"));
+    assertThat(url, containsString("ie"));
+  }
 
-	@Test
-	public void testAddressUrlNoParameterPassing() throws Exception {
+  @Test
+  public void testAddressUrlNoParameterPassing() throws Exception {
 
-		URI uri = uriBuilder.generateAddressURIFromUrl(addressLookupHost + addressLookupPath, "apiKey", "ie",
-				"eircodeOrAddressFrag", null, null, null, null, null, null, null, null, null);
-		String url = uri.toString();
+    URI uri =
+        uriBuilder.generateAddressURIFromUrl(
+            addressLookupHost + addressLookupPath,
+            "apiKey",
+            "ie",
+            "eircodeOrAddressFrag",
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
+    String url = uri.toString();
 
-		assertThat(url, containsString("apiKey"));
-		assertThat(url, containsString("ie"));
-		assertThat(url, containsString("eircodeOrAddressFrag"));
-	}
+    assertThat(url, containsString("apiKey"));
+    assertThat(url, containsString("ie"));
+    assertThat(url, containsString("eircodeOrAddressFrag"));
+  }
 
-	@Test
-	public void testAddressBaseUrlMissing() throws Exception {
-		assertThatThrownBy(() -> uriBuilder.generateAddressURIFromUrl(
-				null, "apikey", "ie", "eircodeOrAddressFrag", "format", 1,
-				0, "include", "exclude", "addtags", "identifier", "callback", true))
+  @Test
+  public void testAddressBaseUrlMissing() throws Exception {
+    assertThatThrownBy(
+            () ->
+                uriBuilder.generateAddressURIFromUrl(
+                    null,
+                    "apikey",
+                    "ie",
+                    "eircodeOrAddressFrag",
+                    "format",
+                    1,
+                    0,
+                    "include",
+                    "exclude",
+                    "addtags",
+                    "identifier",
+                    "callback",
+                    true))
         .isInstanceOf(ConstraintViolationException.class);
-	}
+  }
 
-	@Test
-	public void testAddressApiKeyMissing() throws Exception {
-		assertThatThrownBy(() -> uriBuilder.generateAddressURIFromUrl(
-				addressLookupHost + addressLookupPath, null, "ie", "eircodeOrAddressFrag", "format", 1,
-				0, "include", "exclude", "addtags", "identifier", "callback", true))
+  @Test
+  public void testAddressApiKeyMissing() throws Exception {
+    assertThatThrownBy(
+            () ->
+                uriBuilder.generateAddressURIFromUrl(
+                    addressLookupHost + addressLookupPath,
+                    null,
+                    "ie",
+                    "eircodeOrAddressFrag",
+                    "format",
+                    1,
+                    0,
+                    "include",
+                    "exclude",
+                    "addtags",
+                    "identifier",
+                    "callback",
+                    true))
         .isInstanceOf(ConstraintViolationException.class);
-	}
+  }
 
-	@Test
-	public void testAddressCountryMissing() throws Exception {
-		assertThatThrownBy(() -> uriBuilder.generateAddressURIFromUrl(
-				addressLookupHost + addressLookupPath, "apikey", null, "eircodeOrAddressFrag", "format", 1,
-				0, "include", "exclude", "addtags", "identifier", "callback", true))
+  @Test
+  public void testAddressCountryMissing() throws Exception {
+    assertThatThrownBy(
+            () ->
+                uriBuilder.generateAddressURIFromUrl(
+                    addressLookupHost + addressLookupPath,
+                    "apikey",
+                    null,
+                    "eircodeOrAddressFrag",
+                    "format",
+                    1,
+                    0,
+                    "include",
+                    "exclude",
+                    "addtags",
+                    "identifier",
+                    "callback",
+                    true))
         .isInstanceOf(ConstraintViolationException.class);
-	}
+  }
 }
